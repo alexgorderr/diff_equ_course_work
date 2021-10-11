32dc2aa9b68928c0aeb640d8d06d26d9a383be6d
Mikhail <mihaershv@gmail.com>
2021-10-11 18:39:45 +0300

    gui pickers, code refactoring

---
+++ gui.py
@@ -0,0 +1,78 @@
+from tkinter import ttk
+from tkinter import *
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import forward_euler
+
+
+class Application:
+
+    def __init__(self, window):
+
+        self.window = window
+
+        self.upper_frame = Frame(self.window)
+        self.upper_frame.pack()
+
+        Label(self.upper_frame, text='Steps').grid(column=0, row=0)
+        self.number_of_steps = ttk.Combobox(self.upper_frame,
+                                            values=[1, 2, 3, 4],
+                                            state='readonly')
+        self.number_of_steps.current(0)
+        self.number_of_steps.grid(column=0, row=1)
+
+        Label(self.upper_frame, text='Method').grid(column=1, row=0)
+        self.method_chooser = ttk.Combobox(self.upper_frame,
+                                           values=['Forward Euler',
+                                                   'Backward Euler',
+                                                   'Runge-Kutta',
+                                                   'Heun'],
+                                           state='readonly')
+        self.method_chooser.current(0)
+        self.method_chooser.grid(column=1, row=1)
+
+        Label(self.upper_frame, text='h').grid(column=2, row=0)
+        self.step_chooser = ttk.Combobox(self.upper_frame,
+                                         values=[1, 0.5, 0.1, 0.05, 0.01, 0.005, 0.001],
+                                         state='readonly')
+        self.step_chooser.current(2)
+        self.step_chooser.grid(column=2, row=1)
+
+        self.step_input_button = Button(self.upper_frame, text='Configure', command=self.configure)
+        self.step_input_button.grid(column=4, row=0, rowspan=2)
+        # ----------------
+        self.step_input = Frame(self.window)
+        self.step_input.pack()
+
+        self.evaluate_button = Button(self.step_input, text="Evaluate", command=self.evaluate, state=DISABLED)
+        self.evaluate_button.grid(column=4, row=0, rowspan=2)
+
+        self.graph_frame = Frame(self.window)
+        self.graph_frame.pack()
+
+    def configure(self):
+        self.number_of_steps['state'] = DISABLED
+        self.method_chooser['state'] = DISABLED
+        self.step_chooser['state'] = DISABLED
+        self.step_input_button['state'] = DISABLED
+
+        self.evaluate_button['state'] = NORMAL
+
+        for i in range(int(self.number_of_steps.get())):
+            print('!!!')
+            # elem.grid(column=i, row=0)
+
+    def evaluate(self):
+        steps, v = forward_euler.ForwardEuler().compute()
+
+        fig = Figure(figsize=(6, 6))
+        a = fig.add_subplot(111)
+        a.plot(range(steps), v, 'b-')
+        a.set_title('Euler method')
+        a.set_xlabel('Time, s')
+        a.set_ylabel('Velocity, m/s')
+        # self.graph_frame.master.children.pop()
+        canvas = FigureCanvasTkAgg(fig, master=self.graph_frame)
+        canvas.get_tk_widget().pack()
+        canvas.draw()
+
--- main.py
+++ main.py
@@ -1,63 +1,7 @@
-"""
-    Euler method integrated in GUI
-"""
-
-import numpy as np
-import matplotlib
-from tkinter import *
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
-
-
-class Application:
-
-    def __init__(self, window):
-        self.window = window
-        self.button = Button(window, text="Evaluate", command=self.evaluate)
-        self.button.pack()
-
-        self.g = 9.8
-        self.thrust = 153.51 * 1000  # kN
-        self.engine_burn = 10.3  # s
-        self.specific_impulse = 179.1  # s
-        self.drag_coefficient = 0.38  # kg / m
-        self.consumption_rate = 87.37864  # kg / s
-
-        self.initial_mass = 3380  # kg
-        self.final_mass = self.initial_mass - self.engine_burn * self.consumption_rate
-
-        self.h = 0.5
-
-    def m_t(self, mass, time, lamb):
-        return mass - time*lamb
-
-    def v_t(self, r, m, lam, t, g, k, v):
-        return (r - g * self.m_t(m, t, lam) - k * v * v + lam * v) / self.m_t(m, t, lam)
-
-    def evaluate(self):
-        v = [0]
-
-        steps = int(round(self.engine_burn / self.h, 0))
-
-        for n in range(1, steps):
-            v.append(v[-1] + self.h * self.v_t(self.thrust,
-                                      self.initial_mass,
-                                      self.consumption_rate, n, self.g,
-                                      self.drag_coefficient, v[-1]))
-            print(f"Step: {n}, Xn: {n * self.h}, Vn: {v[-1]}")
-        fig = Figure(figsize=(6, 6))
-        a = fig.add_subplot(111)
-        a.plot(range(steps), v, 'b-')
-        a.set_title('Euler method')
-        a.set_xlabel('Time, s')
-        a.set_ylabel('Velocity, m/s')
-        canvas = FigureCanvasTkAgg(fig, master=self.window)
-        canvas.get_tk_widget().pack()
-        canvas.draw()
-
+import gui
+from tkinter import Tk
 
 if __name__ == '__main__':
     window = Tk()
-    app = Application(window)
+    app = gui.Application(window)
     window.mainloop()
